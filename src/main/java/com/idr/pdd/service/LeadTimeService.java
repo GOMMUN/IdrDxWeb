53846e49ccd11428d4089cb018b51983b67b12c6
JinHong <sna145812@gmail.com>
2023-08-24 09:47:54 +0900

    LEADTIME수정

--- src/main/java/com/idr/pdd/service/LeadTimeService.java
+++ src/main/java/com/idr/pdd/service/LeadTimeService.java
@@ -18,54 +18,24 @@ public class LeadTimeService {
 	@Autowired
 	SimulPerformanceDataMapper mapper;
 
-	public List<LeadTime> find(String start,String end) {
+	public List<LeadTime> find(String start, String end) {
 
-		//Map<String,Integer> count=mapper.sundaycount(start,end);
-		
-		List<LeadTime> list=mapper.leadtime(start, end);
+		// Map<String,Integer> count=mapper.sundaycount(start,end);
 
-		for(LeadTime p:list) {
-			
-			if(Integer.parseInt(p.getLeadTime())>0)
-			{
-				int days = Integer.parseInt(p.getLeadTime()) / (24 * 60 * 60);
-		        int hours = (Integer.parseInt(p.getLeadTime()) % (24 * 60 * 60)) / 3600;
-		        int minutes = (Integer.parseInt(p.getLeadTime()) % 3600) / 60;
-		        int seconds = Integer.parseInt(p.getLeadTime()) % 60;
-		        String result=String.valueOf(days+"일"+hours+"시간"+minutes+"분"+seconds+"초");
-		        p.setLeadTime(result);
-		        
-		        days = Integer.parseInt(p.getLossTime()) / (24 * 60 * 60);
-		        hours = (Integer.parseInt(p.getLossTime()) % (24 * 60 * 60)) / 3600;
-		        minutes = (Integer.parseInt(p.getLossTime()) % 3600) / 60;
-		        seconds = Integer.parseInt(p.getLossTime()) % 60;
-		        result=String.valueOf("+"+days+"일"+hours+"시간"+minutes+"분"+seconds+"초");
-		        p.setLossTime(result);
-		        
-			}else if(Integer.parseInt(p.getLeadTime())<0) {
-				
-				int days = Integer.parseInt(p.getLeadTime()) / (24 * 60 * 60);
-		        int hours = (Integer.parseInt(p.getLeadTime()) % (24 * 60 * 60)) / 3600;
-		        int minutes = (Integer.parseInt(p.getLeadTime()) % 3600) / 60;
-		        int seconds = Integer.parseInt(p.getLeadTime()) % 60;
-		        String result=String.valueOf(days+"일"+hours+"시간"+minutes+"분"+seconds+"초");
-		        p.setLeadTime(result);
-		        
-		        days = Integer.parseInt(p.getLossTime()) / (24 * 60 * 60);
-		        hours = (Integer.parseInt(p.getLossTime()) % (24 * 60 * 60)) / 3600;
-		        minutes = (Integer.parseInt(p.getLossTime()) % 3600) / 60;
-		        seconds = Integer.parseInt(p.getLossTime()) % 60;
-		        result=String.valueOf("-"+days+"일"+hours+"시간"+minutes+"분"+seconds+"초");
-		        p.setLossTime(result);
-			}
-			
-			
+		List<LeadTime> list = mapper.leadtime(start, end);
+
+		for (LeadTime p : list) {
+
+			int days = Integer.parseInt(p.getLeadTime()) / (24 * 60 * 60);
+			int hours = (Integer.parseInt(p.getLeadTime()) % (24 * 60 * 60)) / 3600;
+			int minutes = (Integer.parseInt(p.getLeadTime()) % 3600) / 60;
+			int seconds = Integer.parseInt(p.getLeadTime()) % 60;
+			String result = String.valueOf(days + "일" + hours + "시간" + minutes + "분" + seconds + "초");
+			p.setLeadTime(result);
+
 		}
 
-		
 		return list;
 	}
-	
 
-	
-} 
+}

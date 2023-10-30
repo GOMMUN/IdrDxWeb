12df1565534bb8c8eb7748a2959311121dccb44d
ANCHENGMIN <anchengmin1228@naver.com>
2023-10-30 18:07:10 +0900

    생산일보 Summary 완료

--- src/main/resources/static/js/page/workDailySummary.js
+++ src/main/resources/static/js/page/workDailySummary.js
@@ -2,6 +2,10 @@
  * 
  */
 
+let planqtySum;
+let firsttimegoodqtySum;
+let firsttimefailqtySum;
+
 $(function(){
 	initSetting();
 });
@@ -9,5 +13,48 @@ $(function(){
 function initSetting() {
 	localStorage.setItem("plant", $("#parameterPlant").val());
 	localStorage.setItem("username", $("#parameterUsername").val());
+	
 }
 
+function planqtySumFormatter(data) {
+	field = this.field;
+    planqtySum = data.map(function (row) {
+      return (row[field] ? +row[field] : 0); // 데이터가 null인 경우 0으로 대체
+    }).reduce(function (sum, i) {
+      return sum + i
+    }, 0);
+    
+    return planqtySum;
+}
+
+function firsttimegoodqtySumFormatter(data) {
+	field = this.field;
+	firsttimegoodqtySum =  data.map(function (row) {
+       return (row[field] ? +row[field] : 0); // 데이터가 null인 경우 0으로 대체
+    }).reduce(function (sum, i) {
+      return sum + i
+    }, 0);
+    
+    return firsttimegoodqtySum;
+}
+
+function firsttimefailqtySumFormatter(data) {
+	field = this.field;
+	firsttimefailqtySum = data.map(function (row) {
+       return (row[field] ? +row[field] : 0); // 데이터가 null인 경우 0으로 대체
+    }).reduce(function (sum, i) {
+      return sum + i
+    }, 0);
+    
+    return firsttimefailqtySum;
+}
+
+function percent1Formatter() {
+
+	return ((firsttimegoodqtySum/planqtySum)*100).toFixed(2);
+}
+
+function percent2Formatter() {
+	
+	return ((firsttimefailqtySum/(firsttimefailqtySum+firsttimegoodqtySum))*100).toFixed(2);
+}
